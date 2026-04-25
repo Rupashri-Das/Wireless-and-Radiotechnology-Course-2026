ac41b17676a6aff45adaf670e300da9498b4f465
Rupashri Das <dasrupashri769@gmail.com>
2026-04-25 22:02:46 +0300

    Update socket_sensor.py

--- MQTT-Telegram-Alert-System/socket_sensor.py
+++ MQTT-Telegram-Alert-System/socket_sensor.py
@@ -2,15 +2,15 @@ import socket
 import time
 import random
 
-HOST = "127.0.0.1"
+HOST = "127.0.0.1"   # SAME laptop
 PORT = 5000
 
-s = socket.socket()
-s.connect((HOST, PORT))
+client = socket.socket()
+client.connect((HOST, PORT))
 
 while True:
     temperature = round(random.uniform(20, 35), 2)
-    print("Sending:", temperature)
+    print("Sensor sending:", temperature)
 
-    s.send(str(temperature).encode())
+    client.send(str(temperature).encode())
     time.sleep(2)
